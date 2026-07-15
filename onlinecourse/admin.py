13ba21e924e2b07363210e3572f881bd5c7b439e
baggdits <baggdits@gmail.com>
2026-07-15 12:42:12 -0400

    Update Django final project files

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -11,6 +11,32 @@ from .models import (
     Submission,
 )
 
+class LessonInline(admin.StackedInline):
+		model = Lesson
+		extra = 5
+
+	class ChoiceInline(admin.StackedInline):
+		model = Choice
+		extra = 2
+
+	class QuestionInline(admin.StackedInline):
+		model = Question
+		extra = 2
+
+	# Register your models here.
+	class CourseAdmin(admin.ModelAdmin):
+		inlines = [LessonInline]
+		list_display = ('name', 'pub_date')
+		list_filter = ['pub_date']
+		search_fields = ['name', 'description']
+
+	class QuestionAdmin(admin.ModelAdmin):
+		inlines = [ChoiceInline]
+		list_display = ['content']
+
+	class LessonAdmin(admin.ModelAdmin):
+		list_display = ['title']
+
 # Register your models here.
 
 admin.site.register(Course)
